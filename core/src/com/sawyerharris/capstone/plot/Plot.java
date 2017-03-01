675b1e5474f2fba44863a2b89af02bf585afb9e1
Sawyer Harris <sawyerh99@yahoo.com>
2017-03-01 18:18:52 -0500

    Working on plot scaling and offset

--- core/src/com/sawyerharris/capstone/plot/Plot.java
+++ core/src/com/sawyerharris/capstone/plot/Plot.java
@@ -2,29 +2,59 @@ package com.sawyerharris.capstone.plot;
 
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.utils.ActorGestureListener;
 import com.sawyerharris.capstone.view.ShapeActor;
 
 public class Plot extends ShapeActor {
 	private static final int NUM_POINTS = 100;
 	private static final float PLOT_WIDTH = 400;
 	private float[] vertices;
+	private float[] data;
 
+	private float offset;
+	private float scale;
+	
 	public Plot() {
 		vertices = new float[2 * NUM_POINTS];
+		data = new float[NUM_POINTS];
 		for (int i = 0; i < NUM_POINTS; i++) {
 			vertices[2 * i] = i * PLOT_WIDTH / NUM_POINTS;
 		}
+		
+		scale = 1;
+		
+		setBounds(0, 0, PLOT_WIDTH, PLOT_WIDTH);
+		
+		addListener(new ActorGestureListener() {
+			@Override
+			public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
+				offsetBy(deltaY);
+			}
+		});
 	}
 
 	public void enqueue(int value) {
-		for (int i = 1; i < NUM_POINTS; i++) {
-			vertices[2 * i - 1] = vertices[2 * i + 1];
+		for (int i = 0; i < NUM_POINTS - 1; i++) {
+			data[i] = data[i + 1];
 		}
-		vertices[2 * NUM_POINTS - 1] = value + PLOT_WIDTH / 2;
+		data[NUM_POINTS - 1] = value;
+	}
+	
+	public void setScale(float scale) {
+		this.scale = scale;
+	}
+	
+	public void offsetBy(float amount) {
+		offset += amount;
 	}
 
 	@Override
 	public void drawShapes() {
+		for (int i = 0; i < NUM_POINTS; i++) {
+			vertices[2*i + 1] = data[i]*scale + offset;
+		}
+		
 		renderer.begin(ShapeType.Line);
 		renderer.setColor(Color.BLACK);
 		
