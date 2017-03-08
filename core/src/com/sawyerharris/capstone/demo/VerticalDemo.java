a73474098be1d4f54fbb104f2a7d7c82d64b31a9
Sawyer Harris <sawyerh99@yahoo.com>
2017-03-08 17:57:17 -0500

    Added parametric drive and kapitza buttons

--- core/src/com/sawyerharris/capstone/demo/VerticalDemo.java
+++ core/src/com/sawyerharris/capstone/demo/VerticalDemo.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.ui.Slider;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
@@ -22,13 +23,16 @@ public class VerticalDemo extends Demo {
 	private static final float MASS_BASE = 10f;
 	
 	private float defGravity = 9.8f;
-	private float defLength1 = 1;
+	private float defLength = 3;
 	private float defPsi1 = .01f;
 	private float defOmega1 = 0;
-	private float defMass1 = 1;
-	private float defOmegaDrive = 6.27f;
-	private float defDriveAmplitude = 0.05f;
+	private float defMass = 1;
+	private float defOmegaDrive = 3.614f;
+	private float defDriveAmplitude = 0.02f;
 	private float defDamping = 0;
+	private float defOmegaDriveKapitza = 200;
+	private float defPsi1Kapitza = (float) Math.PI + 0.3f;
+	private float defDriveAmplitudeKapitza = 0.05f;
 	
 	private Pendulum pendulum;
 	private Slider gravitySlider;
@@ -46,6 +50,17 @@ public class VerticalDemo extends Demo {
 	private LinePlot energyPlot;
 	private TextButton angularButton;
 	private TextButton energyButton;
+	private Slider omegaDriveSlider;
+	private Label omegaDriveLabel;
+	private Label omegaDriveValue;
+	private Slider driveAmplitudeSlider;
+	private Label driveAmplitudeLabel;
+	private Label driveAmplitudeValue;
+	private ProgressBar dampingSlider;
+	private Label dampingValue;
+	private Label dampingLabel;
+	private TextButton kapitzaButton;
+	private Actor parametricButton;
 	
 	public VerticalDemo() {
 		////////////////
@@ -54,10 +69,10 @@ public class VerticalDemo extends Demo {
 		
 		simulation = new VerticalOscillationSimulation();
 		simulation.setParameter("gravity", defGravity);
-		simulation.setParameter("length1", defLength1);
+		simulation.setParameter("length1", defLength);
 		simulation.setParameter("psi1", defPsi1);
 		simulation.setParameter("omega1", defOmega1);
-		simulation.setParameter("mass1", defMass1);
+		simulation.setParameter("mass1", defMass);
 		simulation.setParameter("omegaDrive", defOmegaDrive);
 		simulation.setParameter("driveAmplitude", defDriveAmplitude);
 		simulation.setParameter("damping", defDamping);
@@ -65,7 +80,7 @@ public class VerticalDemo extends Demo {
 		simulationWindow = new Group();
 		simulationWindow.setBounds(50, 50, Demo.SIMULATION_WIDTH, Demo.SIMULATION_WIDTH);
 		
-		pendulum = new Pendulum(new Vector2(Demo.SIMULATION_WIDTH/2, Demo.SIMULATION_WIDTH/2), 0, defLength1 * LENGTH_SCALE, MASS_BASE + defMass1 * MASS_SCALE);
+		pendulum = new Pendulum(new Vector2(Demo.SIMULATION_WIDTH/2, Demo.SIMULATION_WIDTH/2), 0, defLength * LENGTH_SCALE, MASS_BASE + defMass * MASS_SCALE);
 		pendulum.addListener(new ActorGestureListener() {
 			@Override
 			public void pan(InputEvent event, float x, float y, float deltaX, float deltaY) {
@@ -117,7 +132,7 @@ public class VerticalDemo extends Demo {
 		
 		// Length parameter
 		lengthSlider = new Slider(1f, 5, 0.1f, false, skin);
-		lengthSlider.setValue(defLength1);
+		lengthSlider.setValue(defLength);
 		lengthSlider.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
@@ -128,7 +143,7 @@ public class VerticalDemo extends Demo {
 			}
 		});
 		lengthLabel = new Label("Length", skin);
-		lengthValue = new Label(String.format("%.1f", defLength1), skin);
+		lengthValue = new Label(String.format("%.1f", defLength), skin);
 		
 		interfaceTable.add(lengthLabel).spaceRight(10);
 		interfaceTable.add(lengthSlider);
@@ -137,7 +152,7 @@ public class VerticalDemo extends Demo {
 		
 		// Mass parameter
 		massSlider = new Slider(1, 10, 1f, false, skin);
-		massSlider.setValue(defMass1);
+		massSlider.setValue(defMass);
 		massSlider.addListener(new ChangeListener() {
 			@Override
 			public void changed(ChangeEvent event, Actor actor) {
@@ -148,13 +163,109 @@ public class VerticalDemo extends Demo {
 			}
 		});
 		massLabel = new Label("Mass", skin);
-		massValue = new Label(String.format("%.0f", defMass1), skin);	
+		massValue = new Label(String.format("%.0f", defMass), skin);	
 		
 		interfaceTable.add(massLabel).spaceRight(10);
 		interfaceTable.add(massSlider);
 		interfaceTable.add(massValue).expandX();
 		interfaceTable.row();
 
+		// Damping parameter
+		dampingSlider = new Slider(0, 1, .1f, false, skin);
+		dampingSlider.setValue(defDamping);
+		dampingSlider.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				float value = ((Slider) actor).getValue();
+				simulation.setParameter("damping", value);
+				dampingValue.setText(String.format("%.1f", value));
+			}
+		});
+		dampingLabel = new Label("Damping", skin);
+		dampingValue = new Label(String.format("%.1f", defDamping), skin);
+		
+		interfaceTable.add(dampingLabel).spaceRight(10);
+		interfaceTable.add(dampingSlider);
+		interfaceTable.add(dampingValue).expandX();
+		interfaceTable.row();
+		
+		// Drive frequency parameter
+		omegaDriveSlider = new Slider(1, 300, 1f, false, skin);
+		omegaDriveSlider.setValue(defOmegaDrive);
+		omegaDriveSlider.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				float value = ((Slider) actor).getValue();
+				simulation.setParameter("omegaDrive", value);
+				omegaDriveValue.setText(String.format("%.0f", value));
+			}
+		});
+		omegaDriveLabel = new Label("Drive Frequency", skin);
+		omegaDriveValue = new Label(String.format("%.0f", defOmegaDrive), skin);
+		
+		interfaceTable.add(omegaDriveLabel).spaceRight(10);
+		interfaceTable.add(omegaDriveSlider);
+		interfaceTable.add(omegaDriveValue).expandX();
+		interfaceTable.row();
+		
+		// Drive amplitude parameter
+		driveAmplitudeSlider = new Slider(0.01f, 0.2f, 0.01f, false, skin);
+		driveAmplitudeSlider.setValue(defDriveAmplitude);
+		driveAmplitudeSlider.addListener(new ChangeListener() {
+			@Override
+			public void changed(ChangeEvent event, Actor actor) {
+				float value = ((Slider) actor).getValue();
+				simulation.setParameter("driveAmplitude", value);
+				driveAmplitudeValue.setText(String.format("%.2f", value));
+			}
+		});
+		driveAmplitudeLabel = new Label("Drive Amplitude", skin);
+		driveAmplitudeValue = new Label(String.format("%.2f", defDriveAmplitude), skin);	
+		
+		interfaceTable.add(driveAmplitudeLabel).spaceRight(10);
+		interfaceTable.add(driveAmplitudeSlider);
+		interfaceTable.add(driveAmplitudeValue).expandX();
+		interfaceTable.row();
+		
+		parametricButton = new TextButton("Parametric Drive", skin);
+		parametricButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				double paraOmegaDrive = 2 * Math.sqrt(simulation.getGravity() / simulation.getLength1());
+				System.out.println(paraOmegaDrive);
+				simulation.setParameter("psi1", defPsi1);
+				simulation.setParameter("omega1", defOmega1);
+				simulation.setParameter("driveAmplitude", defDriveAmplitude);
+				driveAmplitudeSlider.setValue(defDriveAmplitude);
+				simulation.setParameter("omegaDrive", paraOmegaDrive);
+				omegaDriveSlider.setValue((float) paraOmegaDrive);
+				simulation.setParameter("damping", defDamping);
+				dampingSlider.setValue(defDamping);
+			}
+		});
+		
+		kapitzaButton = new TextButton("Kapitza's Pendulum", skin);
+		kapitzaButton.addListener(new ClickListener() {
+			@Override
+			public void clicked(InputEvent event, float x, float y) {
+				simulation.setParameter("psi1", defPsi1Kapitza);
+				simulation.setParameter("omega1", defOmega1);
+				simulation.setParameter("length1", defLength);
+				lengthSlider.setValue(defLength);
+				simulation.setParameter("mass1", defMass);
+				massSlider.setValue(defMass);
+				simulation.setParameter("gravity", defGravity);
+				gravitySlider.setValue(defGravity);
+				simulation.setParameter("omegaDrive", defOmegaDriveKapitza);
+				omegaDriveSlider.setValue(defOmegaDriveKapitza);
+				simulation.setParameter("driveAmplitude", defDriveAmplitudeKapitza);
+				driveAmplitudeSlider.setValue(defDriveAmplitudeKapitza);
+			}
+		});
+		
+		interfaceTable.add(parametricButton);
+		interfaceTable.add(kapitzaButton);
+		
 		interfaceWindow.setBounds(0, 0, Demo.INTERFACE_WIDTH, Demo.INTERFACE_WIDTH);
 		interfaceWindow.addActor(interfaceTable);
 		
@@ -199,7 +310,7 @@ public class VerticalDemo extends Demo {
 		if (PendulumApplication.getInstance().isSimulationRunning()) {
 			pendulum.angle = simulation.getPsi1() - Math.PI / 2;
 			VerticalOscillationSimulation sim = (VerticalOscillationSimulation) simulation;
-			float dy = (float) (LENGTH_SCALE * sim.getDriveAmplitude() * Math.cos(sim.getOmegaDrive() * sim.getT()));
+			float dy = (float) (LENGTH_SCALE * sim.getDriveAmplitude() * -Math.cos(sim.getOmegaDrive() * sim.getT()));
 			pendulum.pivot.y = (Demo.SIMULATION_WIDTH/2 + dy);
 		}
 		angularPlot.addData1((float) (simulation.getPsi1() % (2*Math.PI)));
